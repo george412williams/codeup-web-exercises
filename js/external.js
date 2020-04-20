eefd335bbad2db60a16066fe9b771899eaaa00aa
George Williams <george412williams@gmail.com>
2020-04-20 11:10:24 -0500

    exercises completed

--- js/external.js
+++ js/external.js
@@ -4,45 +4,71 @@ console.log("Hello from external JavaScript");
 
 alert("Welcome to my Website!");
 
-// prompt("What is your favorite color?");
-
 var userInput = prompt("What is your favorite color?");
 
 alert("Neato, " + userInput + " is my favorite color too!");
 
-var dailyRentalRateDollars;
+// var dailyRentalRateDollars;
+//
+// var littleMermaidDaysRented = parseInt(prompt("How long are you renting?"));
+// var brotherBearDaysRented = parseInt(prompt("How long are you renting?"));;
+// var herculesDaysRented = parseInt(prompt("How long are you renting?"));;
+// var dailyRentalRateDollars = 3;
+// var totalRentalCost;
+//
+// alert(totalRentalCost = parseInt((littleMermaidDaysRented + brotherBearDaysRented + herculesDaysRented) * dailyRentalRateDollars));
+// instructor:
+// var totalRentalCost = (parseFloat (lilmerdays....))
+//
+// alert("the total will be $" + totalRentalCost.......)
 
-var littleMermaidDaysRented = parseInt(prompt("How long are you renting?"));
-var brotherBearDaysRented = parseInt(prompt("How long are you renting?"));;
-var herculesDaysRented = parseInt(prompt("How long are you renting?"));;
-var dailyRentalRateDollars = 3;
-var totalRentalCost;
+// toFixed to allow two decimal places x.toFixed(2);
+// parseFloat returns numbers instead of strings, important in this case, without concantenation vs computation
 
-alert(totalRentalCost = parseInt((littleMermaidDaysRented + brotherBearDaysRented + herculesDaysRented) * dailyRentalRateDollars));
 
 //
 
-var googleHourlyRateDollars;
-var amazonHourlyRateDollars;
-var facebookHourlyRateDollars;
-var googleHours;
-var amazonHours;
-var facebookHours;
-var totalPayment;
-totalPayment = googleHours * googleHourlyRateDollars;
-totalPayment += amazonHours * amazonHourlyRateDollars;
-totalPayment += facebookHours * facebookHourlyRateDollars;
+// var googleHourlyRateDollars;
+// var amazonHourlyRateDollars;
+// var facebookHourlyRateDollars;
+// var googleHours;
+// var amazonHours;
+// var facebookHours;
+// var totalPayment;
+// totalPayment = googleHours * googleHourlyRateDollars;
+// totalPayment += amazonHours * amazonHourlyRateDollars;
+// totalPayment += facebookHours * facebookHourlyRateDollars;
+
+//alert("Total payment is $" + totalPayment.toFixed(2));
+//parseFloat(prompt(...
+
 
 //
 
-var classIsNotFull; // boolean
-var classScheduleDoesNotConflict; // boolean
-var studentEnrolled = classIsNotFull && classScheduleDoesNotConflict;
+// var classIsNotFull; // boolean
+// var classScheduleDoesNotConflict; // boolean
+// var studentEnrolled = classIsNotFull && classScheduleDoesNotConflict;
+
+//with boolean instr used, confirm, confirm, yes and yes goes to enrollment, false otherwise(aka else)
+
+
 
 //
 
-var numberOfItems; // number
-var offerIsValid; // boolean
-var isPremiumMember; // boolean
-var productDiscountApplied = offerIsValid && (isPremiumMember || numberOfItems > 2);
+// var numberOfItems; // number
+// var offerIsValid; // boolean
+// var isPremiumMember; // boolean
+// var productDiscountApplied = offerIsValid && (isPremiumMember || numberOfItems > 2);
 
+//refactored by
+//var discountBreakPoint or var numberItemsPerDiscount
+//= 2
+//var numberOfItems - prompt
+//var offerIsNotExp = confirm
+//var isPremMbr = confirm
+
+// var productDisountIsApllied = offerIsNotExpired && (isPremiumMember)
+//
+// alert("Product discount applied: " + productDisountIsApllied);
+
+// Number works as well, if not better than parseInt or parseFloat to render numbers
