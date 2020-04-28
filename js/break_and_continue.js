981847f26c32b15079f025b5adf513b0d5eb7808
George Williams <george412williams@gmail.com>
2020-04-28 15:42:41 -0500

    b and cont .5 compl

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,9 +1,16 @@
 "use strict";
 
 // 2
+var input = prompt("Choose an odd number between 1 and 50");
 
-function chooseOdd1To50() {
-    prompt("Choose an odd number between 1 and 50");
+
+while (true) {
+    if ((input > 50 || input < 1) && input % 2 === 0) {
+        console.log("Invalid Entry");
+    }
+    if (input >= 1 && input <= 49 && input % 2 !==0) {
+        break;
+    }
 }
 
 // 3
