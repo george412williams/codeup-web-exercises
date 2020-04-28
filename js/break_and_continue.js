54c10c619ea6a2e27d0e0e3c6a4428904e431130
George Williams <george412williams@gmail.com>
2020-04-28 15:09:20 -0500

    save point

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,7 +3,7 @@
 // 2
 
 function chooseOdd1To50() {
-
+    prompt("Choose an odd number between 1 and 50");
 }
 
 // 3
--- js/for_loops.js
+++ js/for_loops.js
@@ -3,19 +3,26 @@
 // FOR LOOPS
 
 // 2
+var i = 1;
+
 function showMultiplicationTable(num) {
-    while (i )
+    // while (num <= 10) {
+    //     console.log(num + " x " + i + " = " + (num * i));
+    //     i =+ 1;
+    for (var i = 1; num <= 10; i =+ 1) {
+        console.log(num + " x " + i + " = " + (num * i));
+    }
 }
 
 // 3
 
-for ()
+for (var n = 20; n <= 200)
 
 // 4
 
-for ()
+for (var x = 1; )
 
 // 5
 
-for ()
+for (var d = 100; d >= 5; d - 5);
 
