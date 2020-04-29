78ce2acfe0bb7cd013392160ef31aad63365474a
George Williams <george412williams@gmail.com>
2020-04-29 10:59:56 -0500

    ready for instr input

--- js/while.js
+++ js/while.js
@@ -3,12 +3,15 @@
 // 2
 
 var i = 1;
+
 //var numberToStopAt = 65536;
 
-while (i > 0) {
-    console.log(i);
-    if (i > 10) {
-        console.log(i);
-        break;
-    }
+
+while (i <= 65536) {
+    console.log(i + i);
+    // if (i < 10) {
+    //     console.log(i);
+    //     break;
+    // }
+    // console.log(i);
 }
