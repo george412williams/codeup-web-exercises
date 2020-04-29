9aeec842ad155588858e019a1bbdd292fa27b4ed
George Williams <george412williams@gmail.com>
2020-04-29 10:51:37 -0500

    trying to fix while.js

--- js/while.js
+++ js/while.js
@@ -3,11 +3,12 @@
 // 2
 
 var i = 1;
-var numberToStopAt = 65536;
+//var numberToStopAt = 65536;
 
-while (i => 1) {
-    console.log(i + i);
-    if (i === numberToStopAt) {
+while (i > 0) {
+    console.log(i);
+    if (i > 10) {
+        console.log(i);
         break;
     }
 }
