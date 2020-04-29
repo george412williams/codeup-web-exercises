c47abee9b0529361da820ab2e367d20369392d82
George Williams <george412williams@gmail.com>
2020-04-29 10:34:17 -0500

    final first draft

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,11 +3,11 @@
 // FOR LOOPS
 
 // 2
-// var num = 7;
-//
-// for (var i = 1; i <= 10; i += 1) {
-//     console.log(num + " x " + i + " = " + (num * i));
-// }
+var num = 7;
+
+for (var i = 1; i <= 10; i += 1) {
+    console.log(num + " x " + i + " = " + (num * i));
+}
 
 // function showMultiplicationTable(num) {
 //     for (var i = 1; i <= 10; i += 1) {
@@ -18,21 +18,23 @@
 // 3
 
 // need to add in that it repeats only for 10 numbers
-// for (var n = 20; n <= 200; n + Math.floor(Math.random() * 181)) {
-//     if (n % 2 === 0) {
-//         console.log(n + "is even");
-//     } else {
-//         console.log(n + "is odd");
-//     }
-// }
+for (var n = 20; n <= 200; n + Math.floor(Math.random() * 181)) {
+    if (n % 2 === 0) {
+        console.log(n + "is even");
+    } else {
+        console.log(n + "is odd");
+    }
+}
 
 // 4
 
-// for (var x = 1; x < 10; x++) {
-//     console.log(x.toString().repeat(x));
-// }
+for (var x = 1; x < 10; x++) {
+    console.log(x.toString().repeat(x));
+}
 
 // 5
 
-for (var d = 100; d >= 5; d - 5);
+for (var x = 105; x > 4; x - 5) {
+    console.log(x);
+}
 
