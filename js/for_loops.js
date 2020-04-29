2eb107f175b4859650662deca18db30ffd749538
George Williams <george412williams@gmail.com>
2020-04-29 09:30:52 -0500

    progress check

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,26 +3,27 @@
 // FOR LOOPS
 
 // 2
-var i = 1;
+var num = 7;
 
-function showMultiplicationTable(num) {
-    // while (num <= 10) {
-    //     console.log(num + " x " + i + " = " + (num * i));
-    //     i =+ 1;
-    for (var i = 1; num <= 10; i =+ 1) {
-        console.log(num + " x " + i + " = " + (num * i));
-    }
+for (var i = 1; i <= 10; i += 1) {
+    console.log(num + " x " + i + " = " + (num * i));
 }
+
+// function showMultiplicationTable(num) {
+//     for (var i = 1; i <= 10; i += 1) {
+//         console.log(num + " x " + i + " = " + (num * i));
+//     }
+// }
 
 // 3
 
-for (var n = 20; n <= 200)
-
-// 4
-
-for (var x = 1; )
-
-// 5
-
-for (var d = 100; d >= 5; d - 5);
+// for (var n = 20; n <= 200)
+//
+// // 4
+//
+// for (var x = 1; )
+//
+// // 5
+//
+// for (var d = 100; d >= 5; d - 5);
 
