c7720519b56d107aa06c0b5c1ad8f00ec17bc1d5
George Williams <george412williams@gmail.com>
2020-04-30 09:42:51 -0500

    exercise complete

--- js/iterating.js
+++ js/iterating.js
@@ -6,40 +6,52 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    var names = ['Jack', 'Jill', 'Hansel', 'Gretel'];
+
+var names = ['Jack', 'Jill', 'Hansel', 'Gretel'];
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    console.log(names.length);
+
+console.log(names.length);
+
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    for(var i = 0; i < names.length; i++) {
-        console.log(names[i]);
-    }
+
+for(var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    name.forEach();
+
+names.forEach(function (name) {
+    console.log(name);
+});
+
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
      * return an an element from it
+     *
      * - first: returns the first item in the array
      * - second: returns the second item in the array
      * - last: returns the last item in the array
@@ -49,5 +61,19 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+var numberArray = [1,2,3,4,5];
+
+function returnElement1(arr) {
+    console.log(numberArray[0]);
+    }
+
+function returnElement2(arr) {
+    console.log(numberArray[1]);
+}
+
+function returnElement3(arr) {
+    console.log(numberArray[2]);
+}
 
 })();
