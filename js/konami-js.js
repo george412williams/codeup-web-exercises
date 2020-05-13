8cf161c4edf71f5805057adb235c3c1c18648ed0
George Williams <george412williams@gmail.com>
2020-05-13 15:30:57 -0500

    forming input keycode fxs

--- js/konami-js.js
+++ js/konami-js.js
@@ -1,5 +1,14 @@
 "use strict";
 
+
+// FUTURE CONDITIONAL VARIABLES
+
+var powerOn = true;
+
+var powerOff = false;
+
+// GENERAL VARIABLES
+
 var cargo = {
     inventory: ["box1", "box2"],
     disposition: "loaded",
@@ -14,3 +23,12 @@ var hermaticSeal = {
 function damageReport() {
 
 };
+
+var initiateBurn = ['x','y'];
+
+var initiateLockUnlock = ['keypress1','keypress2'];
+
+var bucket = [];
+
+//bucket.push(event.keycode);
+//if(bucket)
