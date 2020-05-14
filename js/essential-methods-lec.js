ac6995caf8343b603f7eebb7a528b3f3f8245c37
George Williams <george412williams@gmail.com>
2020-05-14 10:39:53 -0500

    notes ongoing

--- js/essential-methods-lec.js
+++ js/essential-methods-lec.js
@@ -31,12 +31,41 @@ $(document).ready(function() {
     // TODO TOGETHER:  Using an event listener and css method, increase the font-size of 'Sorting Hat' when clicked.
     //    How can font size be changed using .html()?
 
+    $('#main-heading').click(function () {
+       //if we are refering to the thing we are referring to we use this, remember?:
+        $(this).css('font-size','4em');
+        //$(this).html('h4 my sorting hat h4')  changes also now the text
+        //$(this).text('p my sorting hat p');  // next 3 lines are how u use html to change font size
+        //$('ul').click(function(){
+        //  $(this).html('<li>Test</li>')
+
+            //NOTE: .prepend() is opposite of .append() method
+
+    });
+
+
     // TODO TOGETHER: store the inner text of #main-heading in a variable
+
+    var mainHeading = $('#main-heading').text();  //just text callnot html
+    alert(mainHeading);
+        // get Sorting Hat as result, .html gets same, but are diff types
 
     // TODO: Using a css method, change the banner background-color to black
     //  Hint: make sure you use the right selector
+    //class of banner applied to that section
+
+    $('.banner').css('background-color','black'); //#000
 
     // TODO TOGETHER: When the 'Houses' button is clicked, add the class 'house' to anything with a class of 'house-name'
+    // adding/removing classes using jquery, bc of using prebuilt styles in css, jquery can be dynamically added/rem'd
+    // me: adding class of emergency procedure to divs that change during an event
+    // click on button with this id
+
+    $('#highlight-houses').click(function(){
+        // target  // you give a string bc addClass takes a string and turns into a class, don't want to make a class out of a class
+        $('.house-name').addClass('house');
+    });
+
 
     // TODO: Comment out the code above. Add the class of 'house' to all the 'house-names'
     //  Write the event listener to remove the class instead
