2ae6002c9096e1a9b3a8186d2830ceeb2ab970e9
George Williams <george412williams@gmail.com>
2020-05-19 10:48:00 -0500

    fxality wther retrieved going to tie weather to map coords

--- js/mapbox-geocode.js
+++ js/mapbox-geocode.js
@@ -21,7 +21,7 @@ function geocode(search, token) {
             return res.json();
             // to get all the data from the request, comment out the following three lines...
         }).then(function(data) {
-            return data.dialy[0].temp.day;
+            return data;  //.features[0].center;
         });
 }
 
