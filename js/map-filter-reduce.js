1e8ae4bd69fbc4d4d56f144b0c88ff39ea65e942
George Williams <george412williams@gmail.com>
2020-05-21 16:48:17 -0500

    half done exercise save

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,62 @@
+"use strict";
+$(document).ready(function() {
+
+    const users = [
+        {
+            id: 1,
+            name: 'ryan',
+            email: 'ryan@codeup.com',
+            languages: ['clojure', 'javascript'],
+            yearsOfExperience: 5
+        },
+        {
+            id: 2,
+            name: 'luis',
+            email: 'luis@codeup.com',
+            languages: ['java', 'scala', 'php'],
+            yearsOfExperience: 6
+        },
+        {
+            id: 3,
+            name: 'zach',
+            email: 'zach@codeup.com',
+            languages: ['javascript', 'bash'],
+            yearsOfExperience: 7
+        },
+        {
+            id: 4,
+            name: 'fernando',
+            email: 'fernando@codeup.com',
+            languages: ['java', 'php', 'sql'],
+            yearsOfExperience: 8
+        },
+        {
+            id: 5,
+            name: 'justin',
+            email: 'justin@codeup.com',
+            languages: ['html', 'css', 'javascript', 'php'],
+            yearsOfExperience: 9
+        }
+    ];
+
+let languageHatTrick = languages.filter((language) => language.length >= 3);
+
+let emailList = users.map((email) => `[${email}]`);
+
+//.reduce to get total # yrs exp, use this to find the avg
+
+
+
+//.reduce to get longest email from users list
+
+
+
+//.reduce to get the list of user's names in a single string: 'Your instructors are: ...names...
+
+
+
+
+
+
+    // JS IFEE END
+});
