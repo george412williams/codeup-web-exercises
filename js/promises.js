61b4c4cd30cffb7cfe60cbfc532fc2c6f7f9b262
George Williams <george412williams@gmail.com>
2020-05-22 16:45:35 -0500

    would like just one thing to work here

--- js/promises.js
+++ js/promises.js
@@ -1,18 +1,28 @@
 "use strict";
 
-var githubAPI = fetch('https://api.github.com', {
-    headers: {
-        'Authorization': `token ${GITHUB_API_KEY}`
-        //make sure key is added as a string
-    }
-});
+// var githubAPI = fetch('https://api.github.com', {
+//     headers: {
+//         'Authorization': `token ${GITHUB_API_KEY}`
+//         //make sure key is added as a string
+//     }
+// });
         //remember, this can be listed in key.js so they are not accessible through this file
     //in html, link js/keys.js if token is not on this page and refer: `token ${TOKEN_NAME}`
 
-githubAPI.then((response) => response.json())
-    .then(jsonResponse => (jsonResponse.results))
-    .then(commitLast => commitLast.get(commit))
-    .catch(error => console.log(error));
+// githubAPI.then((response) => response.json())
+//     .then(jsonResponse => (jsonResponse.results))
+//     .then(commitLast => commitLast.get(commit))
+//     .catch(error => console.log(error));
+//
+// function lastCommitDate() {
+//     var githubAPI = fetch('https://api.github.com', {
+//         headers: {
+//             'Authorization': `token ${GITHUB_API_KEY}`
+//         }
+//     })
+//     lastCommitDate.then()
+//
+// };
 
 // function wait(num) {
 //     return new Promise((resolve, reject) => {
