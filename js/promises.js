cde179e8e9fd1ce26234d3169711744c3f5f1ac9
George Williams <george412williams@gmail.com>
2020-05-22 15:44:31 -0500

    exercise setup and one done

---
+++ js/promises.js
@@ -0,0 +1,21 @@
+"use strict";
+$(document).ready(function() {
+
+
+var githubAPI = fetch('https://api.github.com', {
+    headers: {
+        'Authorization': 'f54ce9c697c230dbc550b985e7638b57cd0a0a7a'
+        //make sure key is added as a string
+    }
+});
+        //remember, this can be listed in key.js so they are not accessible through this file
+    //in html, link js/keys.js if token is not on this page and refer: `token ${TOKEN_NAME}`
+
+
+
+
+
+
+
+// JS IFEE END
+});
