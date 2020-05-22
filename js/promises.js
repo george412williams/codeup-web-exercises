0d18e17552e8a1a85f679cc65be3892e0508a1b2
George Williams <george412williams@gmail.com>
2020-05-22 17:05:02 -0500

    reorgd for fx

--- js/promises.js
+++ js/promises.js
@@ -1,28 +1,21 @@
 "use strict";
 
-// var githubAPI = fetch('https://api.github.com', {
-//     headers: {
-//         'Authorization': `token ${GITHUB_API_KEY}`
-//         //make sure key is added as a string
-//     }
-// });
-        //remember, this can be listed in key.js so they are not accessible through this file
-    //in html, link js/keys.js if token is not on this page and refer: `token ${TOKEN_NAME}`
-
-// githubAPI.then((response) => response.json())
-//     .then(jsonResponse => (jsonResponse.results))
-//     .then(commitLast => commitLast.get(commit))
-//     .catch(error => console.log(error));
-//
 // function lastCommitDate() {
-//     var githubAPI = fetch('https://api.github.com', {
+//     var githubAPI = fetch(`https://api.github.com/users/${username}/events`, {
 //         headers: {
 //             'Authorization': `token ${GITHUB_API_KEY}`
 //         }
 //     })
-//     lastCommitDate.then()
+//
 //
 // };
+
+//find type in the array of events
+//organized from older to most recent, want to look at type of property of each event
+//consider how you might filter out certain events
+//look for type property of commit event or push event
+//new Date()
+//once date, look at date obj in JS and toDateString()
 
 // function wait(num) {
 //     return new Promise((resolve, reject) => {
@@ -31,6 +24,28 @@
 //         })
 //     })
 // }
+
+var githubAPI = fetch('https://api.github.com/users/your-github-username', {
+    headers: {
+        'Authorization': `token ${GITHUB_API_KEY}`
+        //make sure key is added as a string
+    }
+});
+console.log(githubAPI);
+githubAPI.then((response) => response.json())
+    // .then(commitDateArray => commitDateArray.forEach(commitDate => commitDate.))
+        //remember, this can be listed in key.js so they are not accessible through this file
+    //in html, link js/keys.js if token is not on this page and refer: `token ${TOKEN_NAME}`
+
+// githubAPI.then((response) => response.json())
+//     .then(jsonResponse => (jsonResponse.results))
+//     .then(commitLast => commitLast.get(commit))
+//     .catch(error => console.log(error));
+//
+
+
+//
+
 
 //BONUS
 
