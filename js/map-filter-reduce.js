fbe3385ce01a77b282700dfdf326a928dfdafef6
George Williams <george412williams@gmail.com>
2020-05-22 10:14:27 -0500

    finished exercise but not all correct

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -45,7 +45,16 @@ let emailList = users.map((email) => `[${email}]`);
 
 //.reduce to get total # yrs exp, use this to find the avg
 
-let combinedExp = users.filter((yearsExp) )
+// let combinedExp = users.filter((lanuages) => languages += languages);
+// let avgExp = users.reduce((languages) => (combinedExp / combinedExp.length);
+    let combinedExp = users.map((language) => {
+        //changes are defined here with this return
+        return language += language;
+    }, 0);
+
+    // let avgExp = users.map(average)=> {
+    //     return combinedExp / combinedExp.length;
+    // };
 
 //.reduce to get longest email from users list
 
@@ -53,10 +62,16 @@ let longestEmail = users.reduce((longEmail, user) => `${longEmail}${users.name} 
 
 //.reduce to get the list of user's names in a single string: 'Your instructors are: ...names...
 
+//let instructors = users.filter((name) => {
+//  return names;
+// }, 'Your instructors are: ';
+
+//BONUS
 
 
 
 
 
-    // JS IFEE END
+
+ // JS IFEE END
 });
