44cf64144dc168c8b56d8460879491913bcf8366
George Williams <george412williams@gmail.com>
2020-05-22 14:40:04 -0500

    mid lec save

--- js/promises-lec-notes.js
+++ js/promises-lec-notes.js
@@ -5,6 +5,56 @@ $(document).ready(function() {
 
     //Similar to ajax and get, gives more syntactic sugar
 
+    //review on asynchronous, loading not in order, dependant on the source and travel time etc...
+
+    //analogy: cooking
+
+    //start                                                 end
+
+    //boiling water      chop potatoes     chop onions      cook
+
+    // asynch:
+
+    //start                             end
+
+    //boil         ->            cook
+
+    //          up and rt           up
+
+    //chop          ->          chop onions
+
+        //so you can cook faster this way, like i do in the kitchen
+
+    //STATES
+
+        //3 states: Pending / RESOLVED / REJECTED
+
+        //fetch(... when we fetch like a get request to an api; upon running the fetch,
+            //the promise is pending
+            // .then is when it successful
+            // .catch
+            // see in the geocode utils file for the map api to see promises in action
+
+            //to illustrate searched for apis not needing an authentication or an acct
+                //so went with pokemon and SW
+
+            //.then takes in a callback fx
+            pokemonPromise.then(response => response);
+        //why bad to console.log here? bc after the console.log you cannot change properties
+            //so return response into a json data format
+            //so if request is successful it gives the data of the response
+            //the data we get back has not been parsed as JSON
+            //in the properties in the JSON property (in curric it refers) for data able to manip
+
+    //DIFF btwn .get and .fetch
+
+        // chaining - bc the return is a promise, can chain .thens to get to piece of info wo multiple requests
+
+            //resolving with a .then, can take another .then
+
+
+
+
 
 
 
--- js/promises-lec.js
+++ js/promises-lec.js
@@ -5,12 +5,54 @@
  ******************************************** */
 
 var pokemonPromise = fetch('https://pokeapi.co/api/v2/pokemon');
+//return from fetch request is a Promise object   pending
 
 //TODO: console log the results of the above Promise. What is the result?
 
+console.log(pokemonPromise);
+
+
 //TODO: add a method that runs if the Promise resolves successfully
+//.then takes in a callback fx
+pokemonPromise.then(() => console.log("api request successful path"))//.catch(() => console.log("api request fail path"));
+//also
+pokemonPromise.then((response) => response.json());   //just returning response  wo .jason gives raw unmanip data
+
+//pokemonPromise.catch(() => console.log("api request fail path"));
+//an error in the fetch shows in as custom error in console debugger as rejections
+
+// why bad to console.log here? bc after the console.log you cannot change properties
+    //so return response into a json data format
+        //so if request is successful it gives the data of the response
+        //the data we get back has not been parsed as JSON
+    //in the properties in the JSON property (in curric it refers) for data able to manip
+
+pokemonPromise.then((response) => response.json())//parsed into json data object, able to manip now
+    .then(jsonResponse => (jsonResponse.results))       //you will see a results property in this data
+
+    //we're getting the data we want now
+        //so you can call the api once and hang out and grab specific pieces
+    //important to keep track of what layer you're at when requesting, console.log to be sure
+    //so what is the shape of the data right now? like an array of objects, then you know how to iterate through the data, it is
+        //what the current layer of data looks like
+    .then(pokemonArray => pokemonArray.forEach(pokemon => console.log(pokemon.name))) //shows name property of all the object,
+        // pass this statement once when reading through for notes to make better sense
+        //the return from this first fx is passed as a parameter in the second one
+    //to clean, they are all callback fxs,
+    //if have a fx to listNames(arr) {arr.forEach(element => console.log(arr.name));
+        //how can i make a reusable piece of code to plug in
+    // function listNames(arr) {
+    //     arr.forEach(element => console.log(element.name));
+    // };
+    //.then(listNames)  //use this with the fx listNames and not use the 2nd .then statement
+
+    .catch(error => console.log(error));
+//the return from this first fx, is passed as a parameter in the second one
 
 //TODO: add a method that runs if the Promise fails
+
+pokemonPromise.then(() => console.log("api request successful path")).catch(() => console.log("api request fail path"));
+
 
 //TODO: In the callback function of the .then method, parse the response into JSON
 
