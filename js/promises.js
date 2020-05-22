17a143644117b49fbe172e9bdc973d7568b45438
George Williams <george412williams@gmail.com>
2020-05-22 16:28:14 -0500

    one more problem plus bonus keys working

--- js/promises.js
+++ js/promises.js
@@ -1,21 +1,34 @@
 "use strict";
-$(document).ready(function() {
-
 
 var githubAPI = fetch('https://api.github.com', {
     headers: {
-        'Authorization': `token ${TOKEN_NAME}`
+        'Authorization': `token ${GITHUB_API_KEY}`
         //make sure key is added as a string
     }
 });
         //remember, this can be listed in key.js so they are not accessible through this file
     //in html, link js/keys.js if token is not on this page and refer: `token ${TOKEN_NAME}`
 
+githubAPI.then((response) => response.json())
+    .then(jsonResponse => (jsonResponse.results))
+    .then(commitLast => commitLast.get(commit))
+    .catch(error => console.log(error));
+
+// function wait(num) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if ()
+//         })
+//     })
+// }
+
+//BONUS
 
 
 
 
 
 
-// JS IFEE END
-});
+
+
+
