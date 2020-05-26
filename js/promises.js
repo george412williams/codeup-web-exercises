565c7f5c7b6b80b959a9150918e83c35e7e7736a
George Williams <george412williams@gmail.com>
2020-05-26 11:42:19 -0500

    adjusting keys

--- js/promises.js
+++ js/promises.js
@@ -11,35 +11,21 @@ var githubAPI = fetch(`https://api.github.com/users/your-github-username`, {
 
 //console.log(githubAPI);
 
-githubAPI.then((response) => response.json()).catch((error) =>{
-    console.log(error)
-})
 
 
-// function lastCommitDate() {
-//     var githubAPI = fetch(`https://api.github.com/users/${username}/events`, {
-//         headers: {
-//             'Authorization': `token ${GITHUB_API_KEY}`
-//         }
-//     })
-//
-// };
+function lastCommitDate(userName) {
+    let githubAPI = fetch(`https://api.github.com/users/${username}/events`, {
+        headers: {
+            'Authorization': `token ${GITHUB_API_KEY}`
+        }
+    })
+    githubAPI.then((response) => response.json()).catch((error) =>{
+        console.log(error)
+    })
 
-//find type in the array of events
-//organized from older to most recent, want to look at type of property of each event
-//consider how you might filter out certain events
-//look for type property of commit event or push event
-//new Date()
-//once date, look at date obj in JS and toDateString()
+};
 
-// function wait(num) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if ()
-//         })
-//     })
-// }
-
+lastCommitDate(userName, githubAPI).then(console.log);
 
     // .then(commitDateArray => commitDateArray.forEach(commitDate => commitDate.))
         //remember, this can be listed in key.js so they are not accessible through this file
@@ -50,9 +36,35 @@ githubAPI.then((response) => response.json()).catch((error) =>{
 //     .then(commitLast => commitLast.get(commit))
 //     .catch(error => console.log(error));
 //
+//find type in the array of events
+//organized from older to most recent, want to look at type of property of each event
+//consider how you might filter out certain events
+//look for type property of commit event or push event
+//new Date()
+//once date, look at date obj in JS and toDateString()
+
+//============Instructor walkthrough=============
+
+//github personal access token copied; first check gitignore to double check keys.js
+
+
+
+
+//==============================================
 
 //BONUS
 
+// let hesitate = num => {
+//     num = num * 1000;
+//         return ...
+// function wait(num) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if ()
+//         })
+//     })
+// }
+// }
 
 
 
